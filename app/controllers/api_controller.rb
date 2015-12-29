0453fefc296f0cc619f93f1df2accb54ea228a7d
James <JamesCarlson2@hotmail.com>
2015-12-29 10:46:35 -0600

    API user auth complete

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,13 @@
+class ApiController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  private
+  def authenticated?
+    authenticate_or_request_with_http_basic  do |username, password|
+      resource = User.find_by_email(username)
+      if resource.valid_password?(password)
+        sign_in :user, resource
+      end
+    end 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  namespace :api, defaults: {format: :json} do
+    resources :users
+  end
+
   root to: 'home#index'
 
 end
